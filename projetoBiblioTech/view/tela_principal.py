19c3ca5b7aa4e46318101ccac0421072a8e2954c
agnaldo <gui.agnaldo@gmail.com>
2023-06-21 21:07:17 -0300

    Atualização

--- projetoBiblioTech/view/tela_principal.py
+++ projetoBiblioTech/view/tela_principal.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QHeaderView
 from PySide6.QtGui import QPixmap, QPicture
 
 from os import path
@@ -13,3 +13,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+        self.qst_telas.setCurrentWidget(self.pag_procurar_livro)
+        self.tbl_livros.horizontalHeader()
+        self.tbl_livros.itemChanged.connect(self.ajusteTabela)
+
+
+    def ajusteTabela(self):
+        self.tbl_livros.resizeColumnsToContents()
+        self.tbl_livros.resizeRowsToContents()
+        for row in range(self.tbl_livros.rowCount()):
+            height = self.tbl_livros.rowHeight(row)
+            self.tbl_livros.setRowHeight(row, height + 7)
